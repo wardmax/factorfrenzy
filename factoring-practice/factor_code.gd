544b15b6d634fd247c0c41278ca641e917c3331a
wardmax <96925365+wardmax@users.noreply.github.com>
2025-09-27 20:52:44 -0500

    d

--- factoring-practice/factor_code.gd
+++ factoring-practice/factor_code.gd
@@ -4,18 +4,18 @@ var rng = RandomNumberGenerator.new()
 @onready var textbox = $Control/TextEdit
 @onready var label = $Control/Label
 @onready var submit = $Control/Button
+@onready var correct_label = $Control/Label2
+@onready var win_audio = $win_audio
+@onready var loss_audio = $loss_audio
 
 var answer = 0
+var got_correct = false
+var start = 0
+var got_incorrect = false
+
 func _ready():
 	pick_numbers()
 	
-func _input(event: InputEvent) -> void:
-	if(Input.is_action_just_pressed("enter")):
-		textbox.text = textbox.text.replace(" ","")
-		if(answer == textbox.text):
-			print("YAY")
-			textbox.text = ""
-			pick_numbers()
 	
 func pick_numbers():
 	var c1 = 0
@@ -40,6 +40,34 @@ func pick_numbers():
 	answer = "("+str(c1)+"x+"+str(f1)+")("+str(c2)+"x+"+str(f2)+")"
 	print(answer)
 	
+func _on_button_pressed() -> void:
+	textbox.text = textbox.text.replace(" ","")
+	if(answer == textbox.text):
+		got_correct = true
+		win_audio.play()
+		RenderingServer.set_default_clear_color("276221")
+		textbox.text = ""
+		pick_numbers()
+	else:
+		got_incorrect = true
+		loss_audio.play()
+		RenderingServer.set_default_clear_color("630000")
+		
+
+func _process(delta: float) -> void:
+	if(start > .8):
+		start = 0
+		got_correct = false
+		got_incorrect = false
+	if(got_correct):
+		start += delta
+		RenderingServer.set_default_clear_color("276221")
+	elif(got_incorrect):
+		start += delta
+		RenderingServer.set_default_clear_color("630000")
+	else:
+		RenderingServer.set_default_clear_color("4d4d4d")
+
 func is_prime(n: int) -> bool:
 	if n < 2:
 		return false
@@ -54,12 +82,3 @@ func is_prime(n: int) -> bool:
 			return false
 		i += 2
 	return true
-
-
-func _on_button_pressed() -> void:
-	textbox.text = textbox.text.replace(" ","")
-	if(answer == textbox.text):
-		
-			print("YAY")
-			textbox.text = ""
-			pick_numbers()
