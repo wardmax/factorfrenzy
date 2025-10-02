6d5733015ab14f5ee65e628c31b1e1ac798d5f6f
wardmax <96925365+wardmax@users.noreply.github.com>
2025-10-01 21:42:33 -0500

    fixed and reorganized lots

---
+++ factoring-practice/scripts/factor_code.gd
@@ -0,0 +1,121 @@
+extends Node
+
+var rng = RandomNumberGenerator.new()
+@onready var textbox = $Control/TextEdit
+@onready var label = $Control/Label
+@onready var submit = $Control/Button
+@onready var correct_label = $Control/Label2
+@onready var win_audio = $win_audio
+@onready var loss_audio = $loss_audio
+@onready var options_menu = $Control/CheckBox
+
+var popup_options = 0
+
+var answers = []
+var got_correct = false
+var start = 0
+var got_incorrect = false
+var possible_a = [1]
+
+
+func _ready():
+	textbox.keep_editing_on_text_submit = true
+	get_and_display_new_problem()
+	
+func get_options():
+	popup_options.hide_on_item_selection = false
+	print(popup_options.get_item_text(1))
+		
+func get_and_display_new_problem():
+	#(c1x + f1)(c2x+f2)
+	var gcf = 1
+	var c1 = 0
+	var c2 = 0
+	if(len(get_tree().get_first_node_in_group("options_scene").get_possible_a()) >0):
+		possible_a = get_tree().get_first_node_in_group("options_scene").get_possible_a()
+	else:
+		possible_a = [1]
+	print(possible_a)
+	var a = possible_a[rng.randi_range(0,len(possible_a)-1)]
+	
+	if(get_tree().get_first_node_in_group("options_scene").include_gcf()):
+		gcf = rng.randi_range(1,3)
+	
+	if(a == 4):
+		c1 = [2,4][rng.randi_range(0,1)]
+		c2 = 4/c1
+		a = c1
+		print(c1,c2)
+	else:
+		c1 = a
+		c2 = 1
+		
+	#pick constants in linear factors
+	var f1 = rng.randi_range(1,10)
+	var f2 = rng.randi_range(1,10)
+	#ensure no extra gcf
+	if(c1 != 1):
+		while(f1%c1 == 0 or c1%f1 == 0): f1 = rng.randi_range(1,10)
+	if(c2 != 1):
+		while(f2%c2 == 0 or c2%f2 == 0):f2 = rng.randi_range(1,10)
+	
+	build_problem(gcf,a,c1,c2,f1,f2)
+	build_answers(gcf,a,c1,c2,f1,f2)
+	
+	print(answers[0])
+
+func build_problem(gcf,a,c1,c2,f1,f2):
+	var a_value = gcf*a
+	var b_value = gcf*c1*f2+c2*f1
+	var c_value = gcf*f1*f2
+	if(a_value==1):
+		label.text = "Factor: x²+"+str(b_value)+"x+"+str(c_value)
+	else:
+		label.text = "Factor: "+str(gcf*a)+"x²+"+str(b_value)+"x+"+str(c_value)
+	
+func build_answers(gcf,a,c1,c2,f1,f2):
+	if(gcf == 1): gcf = ""
+	if(c1 == 1): c1 = ""
+	if(c2 == 1): c2 = ""
+	answers = [str(gcf)+"("+str(c1)+"x+"+str(f1)+")("+str(c2)+"x+"+str(f2)+")",
+				str(gcf)+"("+str(c2)+"x+"+str(f2)+")("+str(c1)+"x+"+str(f1)+")"]
+
+
+
+#UI pieces ===============================
+func _on_button_pressed() -> void:
+	check_answer()
+		
+func check_answer():
+	textbox.text = textbox.text.replace(" ","")
+	if(textbox.text in answers):
+		got_correct = true
+		win_audio.play()
+		RenderingServer.set_default_clear_color("276221")
+		textbox.text = ""
+		get_and_display_new_problem()
+	else:
+		got_incorrect = true
+		loss_audio.play()
+		RenderingServer.set_default_clear_color("630000")
+	textbox.caret_column = textbox.text.length()
+
+func _process(delta: float) -> void:
+	if(Input.is_action_just_pressed("ui_text_submit")):
+		check_answer()
+	if(start > .8):
+		start = 0
+		got_correct = false
+		got_incorrect = false
+	if(got_correct):
+		start += delta
+		RenderingServer.set_default_clear_color("276221")
+	elif(got_incorrect):
+		start += delta
+		RenderingServer.set_default_clear_color("630000")
+	else:
+		RenderingServer.set_default_clear_color("4d4d4d")
+
+
+func _on_problem_options_pressed() -> void:
+	$problem_options.visible = !$problem_options.visible
---
+++ factoring-practice/scripts/problem_options.gd
@@ -0,0 +1,67 @@
+extends Control
+
+var largest_a = 7
+var checked_a = [true]
+
+func _ready():
+	#fill all with false
+	var fill_falses = []
+	fill_falses.resize(largest_a-1)
+	fill_falses.fill(false)
+	checked_a.append_array(fill_falses)
+	$"vbox1/1".button_pressed = true
+	#connect all check boxes
+	for cb in $vbox1.get_children():
+		if cb is CheckBox:
+			cb.toggled.connect(_on_any_checkbox_toggled.bind(cb))
+	get_possible_a()
+
+func get_options():
+	pass
+
+func _on_any_checkbox_toggled(pressed: bool, which: CheckBox) -> void:
+	if pressed:
+		checked_a[int(which.text)-1] = true
+	else:
+		checked_a[int(which.text)-1] = false
+
+func get_possible_a():
+	var pop_a =[]
+	for i in range(len(checked_a)):
+		if(checked_a[i]):
+			pop_a.append(i+1)
+	return pop_a
+	
+func include_gcf():
+	return $vbox2/gcf.button_pressed
+	
+#quick select for checkboxes===================
+func _on_all_pressed() -> void:
+	for cb in $vbox1.get_children():
+		if cb is CheckBox:
+			cb.button_pressed = true
+
+func _on_none_pressed() -> void:
+	for cb in $vbox1.get_children():
+		if cb is CheckBox:
+			cb.button_pressed = false
+
+func _on_prime_pressed() -> void:
+	for cb in $vbox1.get_children():
+		if cb is CheckBox:
+			if(is_prime(int(cb.text))):
+				cb.button_pressed = true
+		
+func is_prime(n: int) -> bool:
+	if n <= 1:
+		return false		
+	if n == 2:
+		return true
+	if n % 2 == 0:
+		return false
+
+	var limit = int(sqrt(n))
+	for i in range(3, limit + 1, 2):
+		if n % i == 0:
+			return false
+	return true
